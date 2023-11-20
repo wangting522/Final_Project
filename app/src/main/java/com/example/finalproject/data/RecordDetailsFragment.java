1c183b185ce1e6af23a38987df2643bedd415734
Ting Wang <wangting539@gmail.com>
2023-11-19 20:48:53 -0500

    finished layout and pages transition

--- app/src/main/java/com/example/finalproject/data/RecordDetailsFragment.java
+++ app/src/main/java/com/example/finalproject/data/RecordDetailsFragment.java
@@ -7,20 +7,20 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
-import com.example.myapplication.ChatMessage;
-import com.example.myapplication.databinding.DetailsLayoutBinding;
+import com.example.finalproject.MyRecord;
+import com.example.finalproject.databinding.DetailsLayoutBinding;
 
-public class MessageDetailsFragment extends Fragment {
-ChatMessage selected;
-    public MessageDetailsFragment(ChatMessage m){
+public class RecordDetailsFragment extends Fragment {
+MyRecord selected;
+    public RecordDetailsFragment(MyRecord m){
         selected = m;
     }
     public View onCreateView( LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         DetailsLayoutBinding binding = DetailsLayoutBinding.inflate(inflater);
 
-        binding.messageText.setText(selected.getMessage());
-        binding.timeText.setText(selected.getTimeSent());
+        binding.messageText.setText(selected.getRecord());
+        binding.timeText.setText(selected.getTimeSave());
         binding.databaseText.setText("ID="+ selected.getId());
         return binding.getRoot();
     }
