34ca2908718333782dd71d84bccf421683f4edf7
YuxuanXie01 <144855160+YuxuanXie01@users.noreply.github.com>
2023-11-21 17:12:17 -0500

    Revert "Finished first draft"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 android {
     namespace = "com.example.finalproject"
     compileSdk = 34
-
     buildFeatures{
         viewBinding=true
     }
@@ -41,7 +40,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.room:room-common:2.6.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
